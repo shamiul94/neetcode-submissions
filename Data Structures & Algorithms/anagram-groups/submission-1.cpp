e97d4f4511dcf31323d230dc6eab0c7bbaa3d293
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2026-07-28 13:41:45 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(const string& s: strs) {
+            string key = s; 
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> anagramLists;
+        for(const auto& pair: mp) {
+            anagramLists.push_back(pair.second);
+        }
+
+        return anagramLists;
+    }
+    
+    vector<vector<string>> groupAnagrams2(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(int i = 0; i < strs.size(); i++) {
+            string s = strs[i]; 
+            sort(s.begin(), s.end());
+            mp[s].push_back(strs[i]);
+        }
+
+        unordered_map<string, vector<string>>::iterator it;
+
+        vector<vector<string>> anagramLists; 
+        for(it = mp.begin(); it != mp.end(); it++) {
+            vector<string> anagramList = it->second;
+            anagramLists.push_back(anagramList);
+        }
+
+        return anagramLists;
+    }
+};
