7556d2b3f09ba1a87d929ad09b0348ea3a5144c4
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2026-07-29 00:20:48 +0100

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);          // stack memory object
+        ListNode* current = &dummy; // pointer that moves
+
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int a = l1 ? l1->val : 0;
+            int b = l2 ? l2->val : 0;
+
+            int sum = a + b + carry;
+
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            carry = sum / 10;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy.next;
+    }
+};
